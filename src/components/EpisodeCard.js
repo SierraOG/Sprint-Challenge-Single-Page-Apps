68c7d1a1e84d70b40a7398e874ca8fc766aa743b
SierraOG <sierrao@utexas.edu>
2019-07-26 11:06:15 -0500

    added semantic ui to episodes

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -1,12 +1,34 @@
 import React from 'react'
 
+import { Card, Icon } from 'semantic-ui-react'
+
+// return (
+    //     <div className = "episode-card">
+    //       <h2>{episode.name}</h2>
+    //       <h3>{episode.episode}</h3>
+    //       <h4>{episode.air_date}</h4>
+    //       <p>{episode.characters.length} characters</p>
+    //     </div>
+    
+    
+    //   )
+
 export default function EpisodeCard ({ episode }) {
+
+  const description = [
+    episode.episode,
+    ' - ',
+    episode.air_date,
+  ].join(' ')
+
   return (
-    <div className = "episode-card">
-      <h2>{episode.name}</h2>
-      <h3>{episode.episode}</h3>
-      <h4>{episode.air_date}</h4>
-      <p>{episode.characters.length} characters</p>
-    </div>
+    <Card>
+      <Card.Content header={episode.name} />
+      <Card.Content description={description} />
+      <Card.Content extra>
+        <Icon name='user' />
+        {episode.characters.length} characters
+      </Card.Content>
+    </Card>
   )
 }
