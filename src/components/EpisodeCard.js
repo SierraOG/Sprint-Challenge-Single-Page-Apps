dc014415fbfc98e48269f9efe8bab53ab3ae22c9
SierraOG <sierrao@utexas.edu>
2019-07-26 11:18:37 -0500

    added routing and links, mvp complete

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -2,17 +2,6 @@ import React from 'react'
 
 import { Card, Icon } from 'semantic-ui-react'
 
-// return (
-    //     <div className = "episode-card">
-    //       <h2>{episode.name}</h2>
-    //       <h3>{episode.episode}</h3>
-    //       <h4>{episode.air_date}</h4>
-    //       <p>{episode.characters.length} characters</p>
-    //     </div>
-    
-    
-    //   )
-
 export default function EpisodeCard ({ episode }) {
 
   const description = [
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import { Card, Icon } from 'semantic-ui-react'
 
-// <div className = "loc-card">
-//   <h2>{loc.name}</h2>
-//   <h3>{loc.type}</h3>
-//   <h4>{loc.dimension}</h4>
-//   <p>{loc.residents.length} residents</p>
-// </div>
 
 export default function LocationCard ({ loc }) {
 
