f62b598d123063dfc0127b972439ab205c9df25b
SierraOG <sierrao@utexas.edu>
2019-07-26 10:24:00 -0500

    did characters component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard ({  char }) {
+  return (
+    <div className = "char-card">
+      <img src = {char.image} alt = {char.name}/>
+      <h2>{char.name}</h2>
+      <p>{char.species} {char.status}</p>
+      <h3>Location: {char.location.name}</h3>
+      <h3>Origin: {char.origin.name}</h3>
+    </div>
+    )
 }
