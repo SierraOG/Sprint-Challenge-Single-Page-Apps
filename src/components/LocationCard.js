242cb40d6a3ceaf08508c30b5d164359e024b582
SierraOG <sierrao@utexas.edu>
2019-07-26 11:03:11 -0500

    added semantic ui to locations

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import { Card, Icon } from 'semantic-ui-react'
+
+// <div className = "loc-card">
+//   <h2>{loc.name}</h2>
+//   <h3>{loc.type}</h3>
+//   <h4>{loc.dimension}</h4>
+//   <p>{loc.residents.length} residents</p>
+// </div>
 
 export default function LocationCard ({ loc }) {
-  // image={image}
+
+  const description = [
+    loc.type,
+    ' - ',
+    loc.dimension,
+  ].join(' ')
+
   return (
-    <div className = "loc-card">
-      <h2>{loc.name}</h2>
-      <h3>{loc.type}</h3>
-      <h4>{loc.dimension}</h4>
-      <p>{loc.residents.length} residents</p>
-    </div>
+    <Card>
+      <Card.Content header={loc.name} />
+      <Card.Content description={description} />
+      <Card.Content extra>
+        <Icon name='user' />
+        {loc.residents.length} residents
+      </Card.Content>
+    </Card>
   )
 }
