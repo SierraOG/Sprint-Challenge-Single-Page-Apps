114abe512ca2c6633fb7cb21c8903d71c4236f8b
SierraOG <sierrao@utexas.edu>
2019-07-26 10:38:49 -0500

    added location component

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export default function LocationCard ({ name, type, dimension, residents }) {
+export default function LocationCard ({ loc }) {
   // image={image}
-  return (<span>todo: location</span>
+  return (
+    <div className = "loc-card">
+      <h2>{loc.name}</h2>
+      <h3>{loc.type}</h3>
+      <h4>{loc.dimension}</h4>
+      <p>{loc.residents.length} residents</p>
+    </div>
   )
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import LocationCard from './LocationCard'
 
 export default function LocationsList() {
+  // TODO: Add useState to track data from useEffect
+  const [locList, setLocList] = useState([])
+  useEffect(() => {
+    // TODO: Add AJAX/API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/location/')
+    .then(data => {
+      console.log(data.data.results)
+      setLocList(data.data.results)
+    })
+  }, [])
+
+  return <section className='location-list grid-view'>
+     {locList.map((loc) => <LocationCard loc={loc}/>)}
+    </section>
 
 }
